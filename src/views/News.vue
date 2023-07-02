c08a938ae1a8b6505eb412587af2517295941701
wujingmy <13866838174@163.com>
2023-07-02 17:27:01 +0800

    feat: 新闻中心

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="news">
-    <banner title="新闻资讯" />
+    <div class="banner">
+      <div>新闻中心</div>
+    </div>
     <div class="news-section" v-loading="loading">
       <div class="news-section-content">
         <div class="content-nav">
@@ -22,8 +24,7 @@
 
         <div class="content-nav-item">
           <div class="item-list" v-for="(item,index) in newsList" :key="index">
-            <div class="item-img" v-lazy:background-image="imgserver + item.Img"></div>
-
+            <img class="item-img" :src="item.Img" alt="">
             <p class="item-list-title">{{item.Title}}</p>
             <p class="item-list-content">{{item.Content}}</p>
             <div class="item-list-more">
@@ -31,8 +32,8 @@
                 class="text-decoration"
                 :to="{ name: 'newsdetails', params: { id: item.Id }}"
               >
-                <img src="../assets/img/sanjiao.png" />
-                <span>more</span>
+                <div class="triangle"></div>
+                <span>MORE</span>
               </router-link>
             </div>
           </div>
@@ -52,18 +53,65 @@ export default {
   data() {
     return {
       loading: true,
-      newsList: [],
-      newstype: 1
+      newsList:[],
+      newsList1: [
+        {
+          Id: '',
+          "Title":"国务院安委会办公室关于2018年上半年全国建筑业安全生产形势的通报",
+          "Img":"http://shkjgw.shkjem.com//imagestore/2018/0808/ef5a8f93-96ba-4e30-a772-b38223235c62.jpg",
+          "Content": '法兰克福记录打发士大夫发的法大师傅顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶答复1打法'
+        },
+        {
+          Id: '',
+          "Title":"国务院安委会办公室关于2018年上半年全国建筑业安全生产形势的通报",
+          "Img":"http://shkjgw.shkjem.com//imagestore/2018/0808/ef5a8f93-96ba-4e30-a772-b38223235c62.jpg",
+          "Content": '法兰克福记录打发士大夫发的法大师傅顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶答复1打法'
+        },
+        {
+          Id: '',
+          "Title":"国务院安委会办公室关于2018年上半年全国建筑业安全生产形势的通报",
+          "Img":"http://shkjgw.shkjem.com//imagestore/2018/0808/ef5a8f93-96ba-4e30-a772-b38223235c62.jpg",
+          "Content": '法兰克福记录打发士大夫发的法大师傅顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶答复1打法'
+        },
+        {
+          Id: '',
+          "Title":"国务院安委会办公室关于2018年上半年全国建筑业安全生产形势的通报",
+          "Img":"http://shkjgw.shkjem.com//imagestore/2018/0808/ef5a8f93-96ba-4e30-a772-b38223235c62.jpg",
+          "Content": '法兰克福记录打发士大夫发的法大师傅顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶答复1打法'
+        },
+        {
+          Id: '',
+          "Title":"国务院安委会办公室关于2018年上半年全国建筑业安全生产形势的通报",
+          "Img":"http://shkjgw.shkjem.com//imagestore/2018/0808/ef5a8f93-96ba-4e30-a772-b38223235c62.jpg",
+          "Content": '法兰克福记录打发士大夫发的法大师傅顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶答复1打法'
+        },
+       ],
+      newstype: 1,
+      newsList2:[
+      {
+          Id: '',
+          "Title":"国务院安委会办公室关于2018年上半年全国建筑业安全生产形势的通报",
+          "Img":"http://shkjgw.shkjem.com//imagestore/2018/0808/ef5a8f93-96ba-4e30-a772-b38223235c62.jpg",
+          "Content": '法兰克福记录打发士大夫发的法大师傅顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶答复1打法'
+        },
+        {
+          Id: '',
+          "Title":"国务院安委会办公室关于2018年上半年全国建筑业安全生产形势的通报",
+          "Img":"http://shkjgw.shkjem.com//imagestore/2018/0808/ef5a8f93-96ba-4e30-a772-b38223235c62.jpg",
+          "Content": '法兰克福记录打发士大夫发的法大师傅顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶答复1打法'
+        },
+      ],
     };
+    
   },
   methods: {
     loadData() {
-      this.loading = true;
+      this.loading = false;
       this.$http
         .get(`News?type=${this.newstype}&num=6`)
         .then(response => {
           //console.log(response);
-          this.newsList = response.data;
+          // this.newsList = response.data;
           this.loading = false;
           //window.console.log(this.newsList);
         })
@@ -76,10 +124,16 @@ export default {
     this.loadData();
   },
   watch: {
-    newstype(type) {
-      window.console.log(type);
-      this.loadData();
-    }
+    newstype:{
+      handler(type){
+        if(type === 1){
+          this.newsList = this.newsList1;
+        }else{
+          this.newsList = this.newsList2;
+        }
+      },
+      immediate: true,
+    },
   }
 };
 </script>
@@ -92,20 +146,27 @@ export default {
 .news {
   width: 100%;
   height: 100%;
-  background-color: #14679f;
+  background-color: #fff;
   position: relative;
   overflow: hidden;
-
+  .banner{
+    height: 532px;
+    background: url(../assets/image/page3/banner.png) no-repeat center;
+    color: #fff;
+    font-size: 45px;
+    font-weight: 400;
+    line-height: 465px;
+    text-align: center;
+  }
   &-section {
     width: 100%;
     //height: 1600px;
 
     &-content {
-      width: 1240px;
+      width: 1330px;
       //height: 1600px;
       margin: 0 auto;
       background-color: #fff;
-      border: 1px solid red;
 
       .content-nav {
         width: 400px;
@@ -116,7 +177,6 @@ export default {
         align-items: center;
         position: relative;
         bottom: 30px;
-        border: 1px solid red;
 
         &-btn {
           width: 50%;
@@ -129,66 +189,86 @@ export default {
         }
 
         .content-nav-active {
-          background-color: red;
+          background-color: #2da1ea;
           color: #fff;
         }
       }
 
       .content-nav-item {
-        width: 1070px;
-        margin: 0 auto;
+        width: 1330px;
+        margin: 0 auto 40px;
         display: flex;
         justify-content: flex-start;
         flex-wrap: wrap;
         //border: 1px solid blue;
 
         .item-list {
-          width: 330px;
-          height: 500px;
+          width: 422px;
+          height: 362px;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
           margin: 10px 10px;
           border: 1px solid #15669e;
-
+          box-sizing: border-box;
+          padding: 16px;
           &:hover {
             border: 1px solid #fff;
             box-shadow: 0 0 5px 2px #bfd3e0;
           }
           .item-img {
-            width: 300px;
-            height: 210px;
-            background-color: #cecece;
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center;
-            background-origin: content-box;
+            display: block;
+            width: 388px;
+            height: 206px;
           }
 
           &-title {
-            width: 300px;
-            height: 60px;
+            width: 388px;
+            height: 24px;
+            box-sizing: border-box;
             color: #15669e;
-            font-size: 22px;
-            padding: 0 10px;
-            margin: 20px 0;
+            font-size: 18px;
+            font-weight: bold;
+            padding: 0 14px;
+            margin: 10px 0;
             overflow: hidden;
             text-overflow: ellipsis;
-            border-left: 1px solid #15669e;
+            position: relative;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 1;
+            -webkit-box-orient: vertical;
+            white-space: normal !important;
+            word-wrap: break-word;
+            &::before{
+              content: ' ';
+              display: block;
+              position: absolute;
+              width: 6px;
+              height: 20px;
+              background: #15669e;
+              left: 0;
+              top: 4px;
+            }
           }
 
           &-content {
-            width: 273px;
-            height: 100px;
-            font-size: 14px;
+            width: 388px;
+            height: 42px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+            font-size: 16px;
+            padding: 0 14px;
+
             color: gray;
 
             // 文本长度处理 begin
             overflow: hidden;
             text-overflow: ellipsis;
             display: -webkit-box;
-            -webkit-line-clamp: 5;
+            -webkit-line-clamp: 2;
             -webkit-box-orient: vertical;
             white-space: normal !important;
             word-wrap: break-word;
@@ -196,18 +276,23 @@ export default {
           }
 
           &-more {
-            width: 273px;
-            padding-top: 20px;
+            width: 388px;
+            box-sizing: border-box;
+            padding: 0 14px;
             display: flex;
             justify-content: flex-start;
             align-items: center;
 
-            img {
-              width: 12px;
-              height: 12px;
+            .triangle {
+                width: 0;
+                height: 0;
+                display: inline-block;
+                border-left: 8px solid #15669e;
+                border-top: 6px solid transparent;
+                border-bottom: 6px solid transparent;
             }
             span {
-              color: #e13834;
+              color: #15669e;
               padding: 0 5px;
             }
           }
